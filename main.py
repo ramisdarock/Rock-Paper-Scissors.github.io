8d62e537b5a5e87d63ec7cac35559e99bfc25bc4
ramisdarock <148644837+ramisdarock@users.noreply.github.com>
2024-05-18 22:23:22 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+
+play(player, quincy, 1000)
+play(player, abbey, 1000)
+play(player, kris, 1000)
+play(player, mrugesh, 1000)
+
+
+play(human, abbey, 20, verbose=True)
+
+
+play(human, random_player, 1000)
+
+
+
+
+main(module='test_module', exit=False)
