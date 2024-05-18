23e55a7616417261d1a3772f5cc20b7ddbb03dee
ramisdarock <148644837+ramisdarock@users.noreply.github.com>
2024-05-18 22:24:05 +0300

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
+from RPS import player
+from unittest import main
+
 
 play(player, quincy, 1000)
 play(player, abbey, 1000)
